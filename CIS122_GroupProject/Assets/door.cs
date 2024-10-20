9ba6e223a7b8eddd8b3ecd3e583559b8d5befc7c
NiloySarker325 <156709908+NiloySarker325@users.noreply.github.com>
2024-10-19 23:15:07 -0500

    added new door Prefab
    the door opens up automatically upon players arrival in the range and closes after leaving the range.

---
+++ CIS122_GroupProject/Assets/door.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Door : MonoBehaviour
+{
+
+
+    private Animator animator;
+    private bool playerInRange = false; // Tracks if player is in range
+
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Check if the player is in range and presses the "F" key
+        if (playerInRange)
+        {
+            animator.SetBool("Open", true); // Open the chest
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInRange = true; // Player is in range
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInRange = false; // Player left the range
+            animator.SetBool("Open", false); // Close the chest
+        }
+    }
+
+
+}
