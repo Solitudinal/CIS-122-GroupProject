a78a7d58e84b57b6834417a559ce1ee6876bc546
NiloySarker325 <156709908+NiloySarker325@users.noreply.github.com>
2024-10-21 21:02:07 -0500

    added the random Enemy encounter code
    The enemy pops up in designated places tagged with "EncounterZone" and the encounter rate is 10%

--- CIS122_GroupProject/Assets/Scripts/PlayerMovement.cs
+++ CIS122_GroupProject/Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,14 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private int speed = 5;
+    [SerializeField] private float encounterRate = 0.10f;  // 10% chance of encounter per movement
+    // This will store whether the player is in an encounter zone
+    private bool isInEncounterZone = false;
+
     private Vector2 movement;
     private Rigidbody2D rb;
     private Animator animator;
+
 
     private void Awake()
     {
@@ -24,6 +29,12 @@ public class PlayerMovement : MonoBehaviour
             animator.SetFloat("Xinput", movement.x);
             animator.SetFloat("Yinput", movement.y);
             animator.SetBool("IsWalking", true);
+
+            // Check for random encounter only if the player is in an encounter area
+            if (isInEncounterZone)
+            {
+                CheckForRandomEncounter();
+            }
 
         }
         else
@@ -50,4 +61,47 @@ public class PlayerMovement : MonoBehaviour
         // option 3 for movement
         //rb.AddForce(movement * speed);
     }
+
+
+    private void CheckForRandomEncounter()
+    {
+        if (Random.value < encounterRate)
+        {
+            TriggerRandomEncounter();
+        }
+    }
+
+    private void TriggerRandomEncounter()
+    {
+        Debug.Log("Random encounter triggered!");
+        // Example: BattleManager.Instance.StartBattle();
+    }
+
+    // This method will be called when the player enters a collider with an "EncounterZone" tag
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("EncounterZone"))
+        {
+            isInEncounterZone = true;
+        }
+    }
+
+    // This method will be called when the player exits the collider with an "EncounterZone" tag
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("EncounterZone"))
+        {
+            isInEncounterZone = false;
+        }
+    }
+
+    /*
+     
+     1. Create a 2D collider (e.g., BoxCollider2D) in scene for each encounter area.
+     
+     2.Set the collider to be a trigger by checking the “Is Trigger” option in the Collider2D component.
+
+      3.Tag the colliders with "EncounterZone".
+
+     */
 }
