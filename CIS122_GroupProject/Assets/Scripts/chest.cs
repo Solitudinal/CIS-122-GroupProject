e13f9bdd0c1fb9ef95eaff869306e2c3b2616a41
Solitudinal <100310764+Solitudinal@users.noreply.github.com>
2024-12-11 12:44:33 -0600

    Created text popup prefab and added it to chest in bio class

--- CIS122_GroupProject/Assets/Scripts/chest.cs
+++ CIS122_GroupProject/Assets/Scripts/chest.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Chest : MonoBehaviour
@@ -9,26 +10,51 @@ public class Chest : MonoBehaviour
     private Animator animator;
     private bool playerInRange = false; // Tracks if player is in range
 
-    [SerializeField] AudioSource openChestSound; 
+    [SerializeField] AudioSource openChestSound;
+
+    [SerializeField] private TMP_Text rewardText; 
+    [SerializeField] private GameObject rewardTextBox; 
+    [SerializeField] private int lettersPerSecond = 15;
+
+    private bool isChestOpened = false;
 
     void Start()
     {
         animator = this.GetComponent<Animator>();
+        rewardTextBox.SetActive(false); // Ensure reward text is hidden initially
     }
 
     void Update()
     {
-        // Check if the player is in range and presses the "F" key
-        if (playerInRange && Input.GetKeyDown(KeyCode.F))
+        if (playerInRange && Input.GetKeyDown(KeyCode.F) && !isChestOpened)
         {
-            animator.SetBool("Open", true); // Open the chest
+            isChestOpened = true; // Prevent re-opening the chest
+            animator.SetBool("Open", true);
 
-            // Play the sound effect for opening the chest
-            if (openChestSound != null && !openChestSound.isPlaying)
+            if (openChestSound != null)
             {
                 openChestSound.Play();
             }
+
+            // TODO: Randomize chest rewards
+            StartCoroutine(DisplayReward("You found an energy drink!"));
         }
+    }
+
+    // Coroutine that handles item text pop-up
+    IEnumerator DisplayReward(string message)
+    {
+        rewardTextBox.SetActive(true); 
+        rewardText.text = ""; 
+
+        foreach (var letter in message)
+        {
+            rewardText.text += letter;
+            yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
+
+        yield return new WaitForSeconds(2f); 
+        rewardTextBox.SetActive(false); 
     }
 
     void OnTriggerEnter2D(Collider2D other)
